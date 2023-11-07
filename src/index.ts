716bbdf8d655b44b3b98e8d0620a7277d9489296
m.vasianin <m.vasianin@viseven.com>
2023-11-07 11:18:40 +0200

    Add Apollo Server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,102 @@
-import express, { Application, Request, Response } from 'express';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
-const app: Application = express();
-const port = 5000;
+interface Book {
+  title: string;
+  name: string;
+}
 
-app.use('/', (req: Request, res: Response) => {
-  res.status(200).send({ data: 'Hello from Ornio AS' });
+interface MutationResponse {
+  code: string;
+  success: boolean;
+  message: string;
+}
+
+interface BookMutationResponse extends MutationResponse {
+  book: Book;
+}
+
+const books: Book[] = [
+  {
+    title: 'The Awakening',
+    name: 'Kate Chopin'
+  },
+  {
+    title: 'City of Glass',
+    name: 'Paul Auster'
+  }
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+    authors: () => books
+  },
+  Mutation: {
+    addBook: (parent, args) => {
+      const response: BookMutationResponse = {
+        code: '200',
+        success: true,
+        message: 'Success',
+        book: {
+          title: args.title,
+          name: args.author
+        }
+      };
+
+      books.push(response.book);
+
+      return response;
+    }
+  },
+  Book: {
+    author: (parent: Book) => {
+      return books.find((author) => author.name === parent.name);
+    }
+  },
+  Author: {
+    books: (parent: Book) => {
+      return books.filter((book) => book.name === parent.name);
+    }
+  }
+};
+
+const typeDefs = `#graphql
+  interface MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+  }
+  type Book {
+    title: String
+    author: Author
+  }
+  type addBookMutationResponse implements MutationResponse {
+    code: String!
+    success: Boolean!
+    message: String!
+    book: Book
+  }
+  type Author {
+    name: String
+    books: [Book]
+  }
+  type Query {
+    books: [Book]
+    authors: [Author]
+  },
+  type Mutation {
+    addBook(title: String, author: String): addBookMutationResponse
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
 });
 
-app.listen(port, () => console.log(`Server is listening on port ${port}!`));
+console.log(`🚀  Server ready at: ${url}`);
